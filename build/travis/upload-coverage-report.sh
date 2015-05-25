92452447404135577723e90cbcf754d3609348b9
mwjames <jamesin.hongkong.1@gmail.com>
2015-05-25 10:19:32 +0200

    Try hhvm for coverage/scrutinizer/ocular

--- build/travis/upload-coverage-report.sh
+++ build/travis/upload-coverage-report.sh
@@ -2,6 +2,7 @@
 set -ex
 
 BASE_PATH=$(pwd)
+MW_INSTALL_PATH=$BASE_PATH/../mw
 
 if [ "$TYPE" == "coverage" ]
 then
@@ -9,6 +10,11 @@ then
 	# composer require satooshi/php-coveralls:dev-master
 	# php vendor/bin/coveralls -v
 
-	wget https://scrutinizer-ci.com/ocular.phar
-	php ocular.phar code-coverage:upload --format=php-clover $BASE_PATH/build/coverage.clover
+	#wget https://scrutinizer-ci.com/ocular.phar
+	#php ocular.phar code-coverage:upload --format=php-clover $BASE_PATH/build/coverage.clover
+	cd $MW_INSTALL_PATH
+
+	composer require 'scrutinizer/ocular=1.1.*' --prefer-source
+	ocular code-coverage:upload --format=php-clover $BASE_PATH/build/coverage.clover
+
 fi
